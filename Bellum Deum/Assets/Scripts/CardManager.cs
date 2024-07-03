e94bd47652af5d3abd9ff1478f8a6bc65144eb0c
Hugator12 <hugoblazquezesquinas@gmail.com>
2024-07-03 09:33:46 +0200

    Cartas escudo y CardManager

---
+++ Bellum Deum/Assets/Scripts/CardManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+
+public class CardManager : MonoBehaviour
+{
+    public void act_num(Carta carta)
+    {
+        carta.num_cartas -= 1;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
