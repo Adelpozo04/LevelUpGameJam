b847f2d637c121be914054dcdaf7417282f6270f
DiegoSanchezAlvarez22 <146394007+DiegoSanchezAlvarez22@users.noreply.github.com>
2024-07-04 18:24:59 +0200

    MenuOpcionesFinal

--- Bellum Deum/Assets/Scripts/MenusManager.cs
+++ Bellum Deum/Assets/Scripts/MenusManager.cs
@@ -53,7 +53,6 @@ public class MenusManager : MonoBehaviour
             _cronoJ1.GetComponent<TimeManager>().enabled = false;
             _cronoJ2.GetComponent<TimeManager>().enabled = false;
         }
-        
     }
 
     public void Continuar(InputAction.CallbackContext context)
@@ -78,59 +77,85 @@ public class MenusManager : MonoBehaviour
                 _cronoJ2.GetComponent<TimeManager>().enabled = true;
                 UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_deckJ2.transform.GetChild(0).gameObject);
             }
+
+            _menuControles.SetActive(false);
+            _menuIconos.SetActive(false);
         }
-        
     }
 
     public void Controles()
     {
-        Debug.Log("Aplico la accion de controles");
         _menuControles.SetActive(true);
         UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_menuControles.transform.GetChild(2).gameObject);
         
         
     }
 
-    public void Iconos(InputAction.CallbackContext context)
+    public void Iconos()
     {
-        if (context.started)
-        {
-            _menuIconos.SetActive(true);
-            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_menuIconos.transform.GetChild(0).gameObject);
-        }
+        _menuIconos.SetActive(true);
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_menuIconos.transform.GetChild(2).gameObject);      
+    }
+
+    public void Sonido()
+    {
         
     }
 
-    public void Sonido(InputAction.CallbackContext context)
+    public void PantallaCompleta()
     {
-        if (context.started)
-        {
-
-        }
-    }
-
-    public void PantallaCompleta(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-        }
-    }
-
-    public void Volver(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            _menuControles.SetActive(false);
-            _menuIconos.SetActive(false);
-        }
         
     }
 
-    public void Salir(InputAction.CallbackContext context)
+    public void Volver()
     {
-        if (context.started)
-        {
-
-        }
+        _menuControles.SetActive(false);
+        _menuIconos.SetActive(false);
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_botones.transform.GetChild(0).gameObject);
     }
+
+    public void LlamadaContinuar()
+    {
+        _menuOpciones.SetActive(false);
+        _eventSystem.GetComponent<InputSystemUIInputModule>().actionsAsset = _ControlJug_Input;
+        _inputManager.GetComponent<PlayerInput>().SwitchCurrentActionMap("J1");
+
+        _inputManager.GetComponent<InputManager>().CambiarEstadoMenu();
+
+        if (_inputManager.GetComponent<InputManager>()._currentPlayer == Players.Player1)
+        {
+            Debug.Log("es j1");
+            _cronoJ1.GetComponent<TimeManager>().enabled = true;
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_deckJ1.transform.GetChild(0).gameObject);
+        }
+        else
+        {
+            Debug.Log("es j2");
+            _cronoJ2.GetComponent<TimeManager>().enabled = true;
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_deckJ2.transform.GetChild(0).gameObject);
+        }
+
+        _menuControles.SetActive(false);
+        _menuIconos.SetActive(false);
+    }
+
+    //public void Salir()
+    //{
+    //    if (/*_currentScene == nombre de la escena de juego*/)
+    //    {
+    //        SceneManager.LoadScene(""/*"nombre de la escena inicial"*/);
+    //    }
+    //    else
+    //    {
+    //        Application.Quit();
+    //    }
+    //}
+
+    //public void EscPorEscenas(InputAction.CallbackContext context)
+    //{
+    //    if (/*_currentScene == nombre de la escena de juego*/)
+    //    {
+    //        AbrirMenuOpciones(context);
+    //    }
+    //}
 }
