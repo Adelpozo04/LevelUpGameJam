0a8e7cf9ee722f5646e48800938a5be0e1b15d5c
Hugator12 <hugoblazquezesquinas@gmail.com>
2024-07-06 11:42:49 +0200

    Mejora avance

--- Bellum Deum/Assets/Scripts/GameManager.cs
+++ Bellum Deum/Assets/Scripts/GameManager.cs
@@ -728,24 +728,18 @@ public class GameManager : MonoBehaviour
     {
         if (p == Players.Player1)
         {
-            int _cantidad1 = 10 + (10 * _mejorafe1);
-            if (_mejorafe1 < 3 && _advanceBarJ1.GetComponent<AdvanceBarComponent>().CheckAdvance(_cantidad1))
             int _cantidad1 = 10 + (10 * _mejoraavan1);
             if (_mejoraavan1 < 3 && _advanceBarJ1.GetComponent<AdvanceBarComponent>().CheckAdvance(_cantidad1))
             {
-                _feJ1.GetComponent<FeComponent>().UpgradeFeAmount();
                 _advanceBarJ1.GetComponent<AdvanceBarComponent>().UpgradeAdvanceIncrement();
             }
         }
         else
         {
 
-            int _cantidad2 = 10 + (10 * _mejorafe2);
-            if (_mejorafe2 < 3 && _advanceBarJ2.GetComponent<AdvanceBarComponent>().CheckAdvance(_cantidad2))
             int _cantidad2 = 10 + (10 * _mejoraavan2);
             if (_mejoraavan2 < 3 && _advanceBarJ2.GetComponent<AdvanceBarComponent>().CheckAdvance(_cantidad2))
             {
-                _feJ2.GetComponent<FeComponent>().UpgradeFeAmount();
                 _advanceBarJ2.GetComponent<AdvanceBarComponent>().UpgradeAdvanceIncrement();
             }
         }
